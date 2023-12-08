03d7dfa40ec314f5e3df6d192045bf341b25fc07
hhh <1340545944@qq.com>
2023-12-08 22:00:53 +0800

    improve test script

--- test/cli.test.js
+++ test/cli.test.js
@@ -4,10 +4,17 @@ import path from 'node:path';
 import { test } from 'node:test';
 import { fileURLToPath } from 'node:url';
 import process from 'node:process';
+import { existsSync, rmdirSync } from 'node:fs';
 
 test('cli functionality', () => {
   process.chdir(path.dirname(fileURLToPath(import.meta.url)));
+
+  if (existsSync('./root/dest-1')) {
+    rmdirSync('./root/dest-1');
+  }
+
   execSync('node ../src/cli.js -c ./root/backup-config.json');
+
   // TODO:
   assert(false, 'unimplemented');
 });
