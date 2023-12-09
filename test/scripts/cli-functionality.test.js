10cfc30788e11bffb4c20e75b9f2802fc5460a40
hhh <1340545944@qq.com>
2023-12-09 19:16:57 +0800

    add test for incremental update

--- test/scripts/cli-functionality.test.js
+++ test/scripts/cli-functionality.test.js
@@ -8,6 +8,7 @@ import {
   setFileStructure,
 } from './common.js';
 import assert from 'node:assert';
+import { rm, writeFile } from 'node:fs/promises';
 
 test('cli functionality', async () => {
   cdTest();
@@ -67,5 +68,33 @@ test('cli functionality', async () => {
     },
   });
 
-  // TODO: add update test
+  await writeFile(`./root/${TEST_FOLDER}/src/file-1.txt`, 'file-1_new');
+  await writeFile(`./root/${TEST_FOLDER}/src/file-2.txt`, 'file-2_new');
+  await rm(`./root/${TEST_FOLDER}/src/foo/bar/baz.txt`);
+  await writeFile(`./root/${TEST_FOLDER}/dest/file-0.txt`, 'file-0_new');
+  await writeFile(
+    `./root/${TEST_FOLDER}/dest/foo/file-2.txt`,
+    'foo/file-2_new',
+  );
+
+  await execAsync('node', [
+    '../src/cli.js',
+    '-c',
+    `./root/${TEST_FOLDER}/backup-config.json`,
+  ]);
+
+  const newDestinationStructure = await getFileStructure(
+    `./root/${TEST_FOLDER}/dest`,
+    ENCODING,
+  );
+  assert.deepStrictEqual(newDestinationStructure, {
+    'file-0.txt': 'file-0_new',
+    'file-2.txt': 'file-2_new',
+    foo: {
+      'file-2.txt': 'foo/file-2_new',
+      bar: {
+        'baz.txt': 'foo/bar/baz',
+      },
+    },
+  });
 });
