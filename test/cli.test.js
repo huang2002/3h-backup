e8bad2596da8069c3e339f50d28cfac963b77085
hhh <1340545944@qq.com>
2023-12-09 11:01:49 +0800

    fix test runner config

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,20 +1,33 @@
 import assert from 'node:assert';
 import { execSync } from 'node:child_process';
-import path from 'node:path';
 import { test } from 'node:test';
-import { fileURLToPath } from 'node:url';
-import process from 'node:process';
-import { existsSync, rmdirSync } from 'node:fs';
+import { existsSync, rmSync } from 'node:fs';
 
-test('cli functionality', () => {
-  process.chdir(path.dirname(fileURLToPath(import.meta.url)));
+test('cli help info', () => {
+  const cliOutput = execSync('node ../src/cli.js --help', {
+    encoding: 'utf-8',
+  });
+  assert.strictEqual(
+    cliOutput,
+    [
+      'A personal backup helper.',
+      '',
+      'Usage:',
+      '  3h-backup [options]',
+      '',
+      'Options:',
+      '  --help, -h              Show help info.',
+      '  --config, -c <path>     The Path to the config file.',
+      '                          Default: 3h-backup.json',
+      '',
+    ].join('\n'),
+  );
+});
 
+test.todo('cli functionality', () => {
   if (existsSync('./root/dest-1')) {
-    rmdirSync('./root/dest-1');
+    rmSync('./root/dest-1', { recursive: true });
   }
 
   execSync('node ../src/cli.js -c ./root/backup-config.json');
-
-  // TODO:
-  assert(false, 'unimplemented');
 });
