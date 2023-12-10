0f2ccf175f9e0cde2f656cc7dda8344b60dd572e
hhh <1340545944@qq.com>
2023-12-10 10:10:11 +0800

    init manual test script

--- test/scripts/manualTest.js
+++ test/scripts/manualTest.js
@@ -1,1 +1,40 @@
-// TODO:
+import process from 'node:process';
+import { TEST_ROOT_DIR, cdTest, setFileStructure } from './common.js';
+import { writeFileSync } from 'node:fs';
+import { execSync } from 'node:child_process';
+
+const TEST_FOLDER = 'test_manual';
+
+cdTest();
+await setFileStructure(TEST_ROOT_DIR, {
+  [TEST_FOLDER]: {
+    '3h-backup.json': JSON.stringify({
+      skipConfirm: false,
+      tasks: [
+        {
+          source: 'data',
+          destination: 'backup',
+          filter: 'union',
+          replace: 'mtime',
+        },
+      ],
+    }),
+
+    data: {
+      'new.txt': 'new',
+      'update.txt': 'outdated',
+    },
+    backup: {
+      'update.txt': 'outdated',
+      'extra.txt': 'extra',
+    },
+  },
+});
+
+process.chdir(`./root/${TEST_FOLDER}`);
+
+writeFileSync('./data/update.txt', 'updated');
+
+execSync('node ../../../src/cli.js', {
+  stdio: 'inherit',
+});
