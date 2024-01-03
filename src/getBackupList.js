71b89ab941e3ea1c9ae7859f4664b4e652a6521c
hhh@ECUST_MATH_503A <huanghaihua2002@qq.com>
2024-01-03 09:35:48 +0800

    fix missing import

--- src/getBackupList.js
+++ src/getBackupList.js
@@ -1,5 +1,6 @@
 import { getFileList } from './getFileList.js';
 import { existsSync } from 'node:fs';
+import { BackupError } from './type.js';
 
 /**
  * @typedef GetBackupListOptions
@@ -16,74 +17,69 @@ import { existsSync } from 'node:fs';
  * @returns {Promise<readonly string[]>} sorted backup file list
  */
 export const getBackupList = async (options) => {
-    const sourceList = await getFileList('', {
-        root: options.sourcePath,
-        listFiles: options.listFiles,
-        encoding: options.encoding,
-    });
-    sourceList.sort();
+  const sourceList = await getFileList('', {
+    root: options.sourcePath,
+    listFiles: options.listFiles,
+    encoding: options.encoding,
+  });
+  sourceList.sort();
 
-    /**
-     * @type {string[]}
-     */
-    const destinationList = [];
-    if (options.filter !== 'source' || options.replace !== 'all') {
-        if (!existsSync(options.destinationPath)) {
-            if (options.filter === 'destination') {
-                throw new BackupError(
-                    'Destination path does not exists: ' +
-                        options.destinationPath,
-                );
-            }
-        } else {
-            await getFileList(
-                '',
-                {
-                    root: options.destinationPath,
-                    listFiles: options.listFiles,
-                    encoding: options.encoding,
-                },
-                destinationList,
-            );
-            destinationList.sort();
-        }
+  /**
+   * @type {string[]}
+   */
+  const destinationList = [];
+  if (options.filter !== 'source' || options.replace !== 'all') {
+    if (!existsSync(options.destinationPath)) {
+      if (options.filter === 'destination') {
+        throw new BackupError(
+          'Destination path does not exists: ' + options.destinationPath,
+        );
+      }
+    } else {
+      await getFileList(
+        '',
+        {
+          root: options.destinationPath,
+          listFiles: options.listFiles,
+          encoding: options.encoding,
+        },
+        destinationList,
+      );
+      destinationList.sort();
+    }
+  }
+
+  /**
+   * @type {readonly string[]}
+   */
+  let filePathList;
+  switch (options.filter) {
+    case 'source': {
+      filePathList = sourceList;
+      break;
     }
 
-    /**
-     * @type {readonly string[]}
-     */
-    let filePathList;
-    switch (options.filter) {
-        case 'source': {
-            filePathList = sourceList;
-            break;
-        }
-
-        case 'destination': {
-            filePathList = destinationList;
-            break;
-        }
-
-        case 'intersection': {
-            const destinationSet = new Set(destinationList);
-            filePathList = sourceList.filter((filePath) =>
-                destinationSet.has(filePath),
-            );
-            break;
-        }
-
-        case 'union': {
-            const destinationSet = new Set(destinationList);
-            filePathList = destinationList
-                .concat(
-                    sourceList.filter(
-                        (filePath) => !destinationSet.has(filePath),
-                    ),
-                )
-                .sort();
-            break;
-        }
+    case 'destination': {
+      filePathList = destinationList;
+      break;
     }
 
-    return filePathList;
+    case 'intersection': {
+      const destinationSet = new Set(destinationList);
+      filePathList = sourceList.filter((filePath) =>
+        destinationSet.has(filePath),
+      );
+      break;
+    }
+
+    case 'union': {
+      const destinationSet = new Set(destinationList);
+      filePathList = destinationList
+        .concat(sourceList.filter((filePath) => !destinationSet.has(filePath)))
+        .sort();
+      break;
+    }
+  }
+
+  return filePathList;
 };
